cccd451a4225874f2325eccb54d799f3313c9143
amavrits <amavrist@gmail.com>
2025-01-29 16:15:47 +0100

    Started repo

---
+++ digitwin/digitwin.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+from pathlib import Path
+import numpy as np
+import pandas as pd
+from abc import ABC, abstractmethod
+
+
+class DigiTwinBase(ABC):
+
+    def __init__(self):
+        pass
+
+
+
+
+if __name__ == "__main__":
+
+    pass
+
---
+++ main/betuwepand/digitwin_betuwepand.py
@@ -0,0 +1,11 @@
+from digitwin.digitwin import DigiTwinBase
+
+
+class DigiTwinBetuwepand(DigiTwinBase):
+
+    pass
+
+
+if __name__ =="__main__":
+
+    pass
